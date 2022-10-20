3f80c52e045915b97f485b1d79fa2af5076934a4
dotuanduong1510 <68288582+dotuanduong1510@users.noreply.github.com>
2022-10-20 12:34:01 +0700

    Add files via upload

---
+++ HibernateEcommerce/src/main/java/com/planning/HibernateEcommerce/HibernateEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.planning.HibernateEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateEcommerceApplication.class, args);
+	}
+
+}
